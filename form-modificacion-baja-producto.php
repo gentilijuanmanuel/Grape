12ce5f63bfb520376bff6864b6f0a03bde99a728
Juan Manuel Gentili <gentilijuanmanuel80974a@gmail.com>
2017-08-20 10:50:57 -0300

    modificaciones en el navbar

--- form-modificacion-baja-producto.php
+++ form-modificacion-baja-producto.php
@@ -1,3 +1,6 @@
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="es">
   <head>
@@ -18,36 +21,46 @@
     $Producto_seleccionado = mysqli_query($link, $vQueryProducto);
     $Producto = mysqli_fetch_assoc($Producto_seleccionado);
     ?>
+    <!-- Navigation -->
     <nav class="navbar navbar-fixed-top navbar-inverse">
-      <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="index.html">Grape</a>
-        </div>
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.php">Grape</a>
+      </div>
 
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="log-in.html">Entrar <span class="sr-only">(current)</span></a></li>
-          </ul>
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="registrarse.html">Registrarse <span class="sr-only">(current)</span></a></li>
-          </ul>
-          <form class="navbar-form navbar-right" action="resultado-busqueda.php" method="post">
-            <div class="form-group">
-              <input type="text" class="form-control" title="Buscar" name="busqueda" placeholder="Buscar whiskies, vinos...">
-            </div>
-            <button type="submit" class="btn btn-default">Buscar</button>
-          </form>
-        </div><!-- /.navbar-collapse -->
-      </div><!-- /.container-fluid -->
-    </nav>
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <?php
+          if(isset($_SESSION['nombre_usuario'])) {
+              echo '<ul class="nav navbar-nav">';
+              echo '<li class="active"><a href="#">'.$_SESSION['nombre_usuario'].'<span class="sr-only">(current)</span></a></li>';
+              echo '</ul>';
+          }
+          else {
+              echo '<ul class="nav navbar-nav">';
+              echo '<li class="active"><a href="log-in.php">Entrar <span class="sr-only">(current)</span></a></li>';
+              echo '</ul>';
+              echo '<ul class="nav navbar-nav">';
+              echo '<li class="active"><a href="registrarse.html">Registrarse <span class="sr-only">(current)</span></a></li>';
+              echo '</ul>';
+          }
+      ?>
+        <form class="navbar-form navbar-right" action="resultado-busqueda.php" method="post">
+          <div class="form-group">
+            <input type="text" class="form-control" title="Buscar" name="busqueda" placeholder="Buscar whiskies, vinos...">
+          </div>
+          <button type="submit" class="btn btn-default">Buscar</button>
+        </form>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+  </nav>
     <br>
     <br>
     <br>
