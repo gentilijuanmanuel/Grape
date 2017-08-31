abf8a9967d4e8544acf27301b59dc66f7dbc30fc
Martin Bianchi <martinbianchi7@outlook.com>
2017-08-31 15:47:06 -0300

    Estilo envio de mail

--- enviar(VIEJO).php
+++
@@ -1,8 +0,0 @@
-<?php
-    $destino= "ivos19@hotmail.com";
-    $asunto= $_POST['asunto'];
-    $desde= $_POST['mail'];
-    $comentario= $_POST['comentario'];
-    mail('$destino', '$asunto', '$comentario', '$desde');
-    echo "Su consulta ha sido enviada, en breve recibirá nuestra espuesta.";
-?>
--- enviar.php
+++ enviar.php
@@ -4,19 +4,19 @@
 	$destino='ivos19@hotmail.com'; /*"micorreo@dominio.com";*/
 	$asunto='Comentario';
 	$desde='From:' . $_POST['email'];
-	
+
 	$mailComprobacion= $_POST['email'];
 
-	$comentario= 
-		
-		'Nombre: '.$_POST[nombre]. "\r\n".
-		'Email: '.$_POST[email]. "\r\n".
-		'Consulta: '.$_POST[texto]. "\r\n".
+	$comentario=
+
+		'Nombre: '.$_POST['nombre']. "\r\n".
+		'Email: '.$_POST['email']. "\r\n".
+		'Consulta: '.$_POST['texto']. "\r\n".
 		'Enviado: '.$fecha .'a las'. $hora;
 
 	mail( $destino , $asunto, $comentario, $desde);
 	mail( $mailComprobacion , 'Confirmacion envio', 'El mail ha sido enviado satifactoriamente');
-	
-	echo "Su consulta ha sido enviada, en breve recibira nuestra
-	respuesta.";
+
+	echo "<h3>Su consulta ha sido enviada, en breve recibira nuestra
+	respuesta.</h3>";
 ?>
