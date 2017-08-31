281dc44e6e75c80c9da191f4e89c5e824a5a72df
Ivos19 <ivossch@gmail.com>
2017-08-31 15:14:32 -0300

    PAgina envio

--- enviar.php
+++ enviar.php
@@ -1,8 +1,22 @@
 <?php
-    $destino= "ivos19@hotmail.com";
-    $asunto= $_POST['asunto'];
-    $desde= $_POST['mail'];
-    $comentario= $_POST['comentario'];
-    mail('$destino', '$asunto', '$comentario', '$desde');
-    echo "Su consulta ha sido enviada, en breve recibirá nuestra espuesta.";
+	$fecha=date("d-m-Y");
+	$hora= date("H :i:s");
+	$destino='ivos19@hotmail.com'; /*"micorreo@dominio.com";*/
+	$asunto='Comentario';
+	$desde='From:' . $_POST['email'];
+	
+	$mailComprobacion= $_POST['email'];
+
+	$comentario= 
+		
+		'Nombre: '.$_POST[nombre]. "\r\n".
+		'Email: '.$_POST[email]. "\r\n".
+		'Consulta: '.$_POST[texto]. "\r\n".
+		'Enviado: '.$fecha .'a las'. $hora;
+
+	mail( $destino , $asunto, $comentario, $desde);
+	mail( $mailComprobacion , 'Confirmacion envio', 'El mail ha sido enviado satifactoriamente');
+	
+	echo "Su consulta ha sido enviada, en breve recibira nuestra
+	respuesta.";
 ?>
